b9a8f871f18432f3987ff21cb0ea604ef0549f28
pcompassion <p.compassion@gmail.com>
2018-08-10 16:53:31 +0900

    f

--- mysite/apps/mpemail/config/logistics.py
+++ mysite/apps/mpemail/config/logistics.py
@@ -1,7 +1,8 @@
 import csv
+import codecs
 
 def get_logistics_info(start_date, end_date):
-    with open('logistics_data.csv') as f:
+    with codecs.open('logistics_data.csv', encoding='EUC-KR') as f:
         csv_reader = csv.reader(f, delimiter=',', quotechar='"')
         data = []
         no = 0
@@ -35,35 +36,33 @@ def get_logistics_info(start_date, end_date):
 
             if one_data['print_date'] >= start_date and one_data['print_date'] <= end_date:
                 data.append(one_data)
-        
+
         return data
 
 
 if __name__ == '__main__':
     data = get_logistics_info('20180716', '20180717')
-    
-    print 'no: %s' % len(data)
-    print 'order_no: %s' % data[3]['order_no']
-    print 'logis_no: %s' % data[3]['logis_no']
-    print 'name: %s' % data[3]['name']
-    print 'home_phone: %s' % data[3]['home_phone']
-    print 'mobile: %s' % data[3]['mobile']
-    print 'safe_phone: %s' % data[3]['safe_phone']
-    print 'product: %s' % data[3]['product']
-    print 'addr: %s' % data[3]['addr']
-    print 'zip: %s' % data[3]['zip']
-    print 'sender_name: %s' % data[3]['sender_name']
-    print 'sender_home_phone: %s' % data[3]['sender_home_phone']
-    print 'sender_mobile: %s' % data[3]['sender_mobile']
-    print 'delivery_status: %s' % data[3]['delivery_status']
-    print 'delivery_detail: %s' % data[3]['delivery_detail']
-    print 'print_date: %s' % data[3]['print_date']
-    print 'delivery_start_date: %s' % data[3]['delivery_start_date']
-    print 'delivery_end_date: %s' % data[3]['delivery_end_date']
-    print 'box_no: %s' % data[3]['box_no']
-    print 'box_type: %s' % data[3]['box_type']
-    print 'price: %s' % data[3]['price']
-    print 'additional_price: %s' % data[3]['additional_price']
-    print 'etc: %s' % data[3]['etc']
-    
-    
+
+    print('no: %s' % len(data))
+    print('order_no: %s' % data[3]['order_no'])
+    print('logis_no: %s' % data[3]['logis_no'])
+    print('name: %s' % data[3]['name'])
+    print('home_phone: %s' % data[3]['home_phone'])
+    print('mobile: %s' % data[3]['mobile'])
+    print('safe_phone: %s' % data[3]['safe_phone'])
+    print('product: %s' % data[3]['product'])
+    print('addr: %s' % data[3]['addr'])
+    print('zip: %s' % data[3]['zip'])
+    print('sender_name: %s' % data[3]['sender_name'])
+    print('sender_home_phone: %s' % data[3]['sender_home_phone'])
+    print('sender_mobile: %s' % data[3]['sender_mobile'])
+    print('delivery_status: %s' % data[3]['delivery_status'])
+    print('delivery_detail: %s' % data[3]['delivery_detail'])
+    print('print_date: %s' % data[3]['print_date'])
+    print('delivery_start_date: %s' % data[3]['delivery_start_date'])
+    print('delivery_end_date: %s' % data[3]['delivery_end_date'])
+    print('box_no: %s' % data[3]['box_no'])
+    print('box_type: %s' % data[3]['box_type'])
+    print('price: %s' % data[3]['price'])
+    print('additional_price: %s' % data[3]['additional_price'])
+    print('etc: %s' % data[3]['etc'])
